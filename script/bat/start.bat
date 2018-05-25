e3c1dd8dd15b3647d399dd01c278f0378abcb4ce
Suvan-L“ <liushuwei0925@gmail.com>
2018-05-25 22:14:38 +0800

    update srcit/bat directory, add 'start.bat'

---
+++ script/bat/start.bat
@@ -0,0 +1,13 @@
+echo "*************************** 正在启动开发环境 **********************************"
+
+start "" "Chrome"
+start "" "listary"
+start "" "Shadowsocks"
+start "" "TranslucentTB"
+start "" "Cmder"
+start "" "YodaoDict"
+start "" "wechat"
+start "" "腾讯QQ"
+start "" "idea64"
+
+exit
